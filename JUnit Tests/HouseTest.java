df4c47175cbea546eb3492ba734114121b379ca1
jjp75 <jjp75@pitt.edu>
2015-02-15 00:49:43 -0500

    Finalized JUnit tests for House.java

--- JUnit Tests/HouseTest.java
+++ JUnit Tests/HouseTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,37 +14,30 @@ public class HouseTest {
 	private House house;
 	
 	/*
-	 * This function is called before each test is run. Create a House instance that has three
-	 * mock Rooms. The first mock Room will have the description Room1, the second mock Room will
-	 * have the description Room2, and the third mock Room will have the description Room3.
+	 * This function initializes a new House instance that has three mock Rooms before each test is run.
+	 * The first mock Room will have the description Room1, the second mock Room will have the 
+	 * description Room2, and the third mock Room will have the description Room3.
 	 */
 	@Before
 	public void setUp() {
 		rooms = new Room[3];
-		
 		rooms[0] = mock(Room.class);
 		rooms[1] = mock(Room.class);
 		rooms[2] = mock(Room.class);
-		
 		when(rooms[0].getDescription()).thenReturn("Room1");
 		when(rooms[1].getDescription()).thenReturn("Room2");
 		when(rooms[2].getDescription()).thenReturn("Room3");
-		
 		house = new House(rooms);
 	}
-	
-	/*
-	 * This function is called after each test is run.
-	 */
-	@After
-	public void tearDown() {}
 	
 	/*
 	 * Test to see if a non-null value is returned when 6 is passed to the House(int numRooms) constructor.
 	 */
 	@Test
 	public void testHouseConstructorNumRooms() {
-		assertNotNull(new House(6));
+		House house = null;
+		house = new House(6);
+		assertNotNull(house);
 	}
 	
 	/*
@@ -53,7 +45,9 @@ public class HouseTest {
 	 */
 	@Test
 	public void testHouseConstructorRooms() {
-		assertNotNull(new House(rooms));
+		House house = null;
+		house = new House(rooms);
+		assertNotNull(house);
 	}
 	
 	/*
@@ -106,46 +100,40 @@ public class HouseTest {
 	}
 	
 	/*
-	 * Test to see if the Player finds coffee.
+	 * Test to see if the Player is able to find coffee.
 	 */
 	@Test
 	public void testLookGetCoffee() {
 		Player mockPlayer = mock(Player.class);
 		Room mockRoom = mock(Room.class);
-		
 		when(mockRoom.hasItem()).thenReturn(true);
 		when(mockRoom.hasCoffee()).thenReturn(true);
-		
 		house.look(mockPlayer, mockRoom);
 		verify(mockPlayer).getCoffee();
 	}
 	
 	/*
-	 * Test to see if the Player finds cream.
+	 * Test to see if the Player is able to find cream.
 	 */
 	@Test
 	public void testLookGetCream() {
 		Player mockPlayer = mock(Player.class);
-		Room mockRoom = mock(Room.class);
-		
+		Room mockRoom = mock(Room.class);	
 		when(mockRoom.hasItem()).thenReturn(true);
 		when(mockRoom.hasCream()).thenReturn(true);
-		
 		house.look(mockPlayer, mockRoom);
 		verify(mockPlayer).getCream();
 	}
 	
 	/*
-	 * Test to see if the Player finds sugar.
+	 * Test to see if the Player is able to find sugar.
 	 */
 	@Test
 	public void testLookGetSugar() {
 		Player mockPlayer = mock(Player.class);
-		Room mockRoom = mock(Room.class);
-		
+		Room mockRoom = mock(Room.class);	
 		when(mockRoom.hasItem()).thenReturn(true);
 		when(mockRoom.hasSugar()).thenReturn(true);
-		
 		house.look(mockPlayer, mockRoom);
 		verify(mockPlayer).getSugar();
 	}
@@ -155,6 +143,8 @@ public class HouseTest {
 	 */
 	@Test
 	public void testGenerateRooms() {
-		assertNotNull(house.generateRooms(6));
+		Room[] rooms = null;
+		rooms = house.generateRooms(6);
+		assertNotNull(rooms);
 	}
 }
